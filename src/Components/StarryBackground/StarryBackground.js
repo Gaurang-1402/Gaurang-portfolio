e8cbb677cd884be19cb16055715cec837d9111f7
Gaurang Ruparelia <gr2159@nyu.edu>
2021-08-08 12:37:59 +0530

    initial commit

---
+++ src/Components/StarryBackground/StarryBackground.js
@@ -0,0 +1,18 @@
+import "./StarryBackground.css"
+
+function StarryBackground() {
+  return (
+    <div className='App'>
+      <link
+        href='https://fonts.googleapis.com/css?family=Lato:300,400,700'
+        rel='stylesheet'
+        type='text/css'
+      />
+      <div id='stars'></div>
+      <div id='stars2'></div>
+      <div id='stars3'></div>
+    </div>
+  )
+}
+
+export default StarryBackground
